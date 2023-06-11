c6b4f15fcec060a38ada78363cb7e125b99d1255
Paulina <71526180+paulinek13@users.noreply.github.com>
2023-06-11 16:44:39 +0200

    initial commit

---
+++ docs.md
@@ -0,0 +1,11 @@
+# priority levels
+- critical - 0
+- high - 1
+- normal - 2
+- low - 3
+- none - 4
+
+# task data
+- title/name
+- priority level
+- id
---
+++ main.js
@@ -0,0 +1,81 @@
+import Dexie from "https://unpkg.com/dexie@latest/dist/modern/dexie.mjs";
+
+const db = new Dexie('todo_db');
+const tasksPriorityNames = ["urgent", "high", "normal", "low", "none"];
+
+db.version(1).stores({tasks: '++id, name, lvl'});
+
+async function renderTasks() {
+    const tasks = await db.tasks.orderBy('lvl').toArray();
+
+    const taskList = document.getElementById('tasks-list');
+    taskList.innerHTML = '';
+  
+    tasks.forEach((task, index) => {
+      const task_div = document.createElement('li');
+      const task_name = document.createElement('span');
+      const task_lvl = document.createElement('span');
+    //   const task_edit_button = document.createElement('button');
+      const task_delete_button = document.createElement('button');
+
+      task_div.classList.add("task-div");
+      task_div.classList.add(`task-lvl-${task.lvl}`);
+
+      task_name.classList.add("task-name");
+      task_lvl.classList.add("task-lvl");
+    //   task_edit_button.classList.add("task-edit-button");
+      task_delete_button.classList.add("task-delete-button");
+
+      task_name.textContent = task.name;
+      task_lvl.textContent = tasksPriorityNames[task.lvl];
+    //   task_edit_button.textContent = 'edit';
+      task_delete_button.textContent = '×';
+
+      task_delete_button.addEventListener('click', () => removeTask(task.id, task.name));
+  
+      task_div.appendChild(task_delete_button);
+    //   task_div.appendChild(task_edit_button);
+      task_div.appendChild(task_name);
+      task_div.appendChild(task_lvl);
+
+      taskList.appendChild(task_div);
+    });
+}
+
+function removeTask(primaryKey, taskName){
+    db.tasks.delete(primaryKey).then(function(res){
+        console.log(`successfully deleted '${taskName}' task`);
+        renderTasks();
+    }).catch(function(error){
+        console.log(`error deleting ${primaryKey} task \n ${error}`);
+    });
+}
+
+function addTask(taskName, taskLvl){
+    db.tasks.add({
+		name: taskName,
+		lvl: parseInt(taskLvl)
+	}).then(function(res){
+        console.log(`successfully added '${taskName}' task`);
+        renderTasks();
+        console.log(document.querySelector('input[name="task_priority"]:checked').value)
+    }).catch(function(error){
+        console.log(`error adding a task \n ${error}`);
+    });
+}
+
+document.getElementById("add-new-task-button").addEventListener('click', () => {
+    document.getElementById("name").value = "";
+    document.getElementById("add-new-task-dialog").showModal();
+    document.getElementById("name").focus();
+});
+
+document.getElementById("cancel-new-task-button").addEventListener('click', () => {
+    document.getElementById("add-new-task-dialog").close();
+});
+
+document.getElementById("new-task-form").addEventListener('submit',
+    () => addTask(document.getElementById("name").value,
+    document.querySelector('input[name="task_priority"]:checked').value));
+
+renderTasks();
